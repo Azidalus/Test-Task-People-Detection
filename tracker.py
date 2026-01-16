dd0b310d0b63ee8aa0655af3f82d2b81d3237cdd
Azidalus <brueva.vi@gmail.com>
2026-01-16 03:55:54 +0300

    updated to botsort + yolo

--- tracker.py
+++ tracker.py
@@ -1,69 +1,5 @@
-import numpy as np
 from pathlib import Path
-import supervision as sv
 from ultralytics import YOLO
-
-detector = YOLO('yolo11m.pt')
-tracker = sv.ByteTrack()
-box_annotator = sv.BoxAnnotator()
-label_annotator = sv.LabelAnnotator()
-
-def callback(frame: np.ndarray, _: int) -> np.ndarray:
-    """
-    Обработчик кадров для детекции и трекинга людей.
-
-    Args:
-    frame (np.ndarray): Входной кадр видео.
-    _: Номер кадра (не используется).
-
-    Returns: 
-    np.ndarray: Аннотированный кадр 
-    с детекциями и трекингом людей.
-    """
-    results = detector(frame)[0]
-    
-    # Преобразовать результаты детекции в формат Supervision
-    detections = sv.Detections.from_ultralytics(results)
-
-    # Выбрать только детекции людей (class_id == 0)
-    person_mask = detections.class_id == 0
-    detections = detections[person_mask]
-    
-    # Обновить трекер с новыми детекциями
-    detections = tracker.update_with_detections(detections)
-
-    labels = [
-        f"person #{tracker_id} {confidence:.2f}"
-        for tracker_id, confidence
-        in zip(detections.tracker_id, detections.confidence)
-    ]
-
-    # Аннотировать кадр с помощью боксов и меток
-    annotated_frame = box_annotator.annotate(
-        scene=frame.copy(),
-        detections=detections
-    )
-    annotated_frame = label_annotator.annotate(
-        scene=annotated_frame,
-        detections=detections,
-        labels=labels
-    )
-
-    return annotated_frame
-
-
-def track_video(src_path, trgt_path):
-    """
-    Запускает процесс трекинга людей в видео.
-
-    src_path: Путь к исходному видеофайлу.
-    trgt_path: Путь к целевому видеофайлу.
-    """
-    sv.process_video(
-        source_path=src_path,
-        target_path=trgt_path,
-        callback=callback
-    )
 
 
 if __name__ == "__main__":
@@ -71,13 +7,21 @@ if __name__ == "__main__":
     # Создание кроссплатформенных путей к файлам с помощью pathlib
     BASE_DIR = Path(__file__).resolve().parent
     source_path = BASE_DIR / "assets" / "crowd.mp4"
-    target_path = BASE_DIR / "outputs" / "crowd_tracked.mp4"
+    output_path = BASE_DIR / "outputs"
 
-    target_path.parent.mkdir(exist_ok=True)
+    output_path.parent.mkdir(exist_ok=True)
 
-    # Запуск трекинга видео
-    track_video(
-        src_path=str(source_path),
-        trgt_path=str(target_path)
+    # Инициализация детектора YOLO и запуск трекинга
+    detector = YOLO('yolo11m.pt')
+    
+    res = detector.track(
+        source_path, 
+        conf=0.4, 
+        iou=0.4, 
+        classes=[0], # Класс 0 соответствует людям
+        save=True, 
+        project=output_path,
+        line_width=2 # Толщина линий для аннотаций
     )
+
     print("Processing complete. Annotated video saved as 'crowd_tracked.mp4'.")
